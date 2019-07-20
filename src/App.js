28b55b8f700da2f48120dde6d6f39b89705ada9a
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-07-20 09:03:44 -0400

    added to App.js and game is working

--- src/App.js
+++ src/App.js
@@ -14,6 +14,67 @@ class App extends Component {
       score: 0,
       highscore: 0
     };
+
+    // The function when the game ends
+    gameOver = () => {
+
+      // if you get all the matches correct you win!
+      if (this.state.score === 12) {
+        this.setState({ highscore: this.state.score }, function () {
+          console.log(this.state.highscore);
+          alert(`You Won! Click to replay! \nscore:  ${this.state.highscore}`);
+        });
+        this.state.cards.forEach(card => {
+          card.count = 0;
+        });
+
+        // score resets
+        this.setState({ score: 0 });
+        this.setState({ highscore: 0 });
+        return true;
+      } else {
+        this.setState({ highscore: this.state.score }, function () {
+          console.log(this.state.highscore);
+        });
+        this.state.cards.forEach(card => {
+          card.count = 0;
+        });
+        alert(`Game Over \nscore:  ${this.state.score}`);
+  
+        this.setState({ score: 0 });
+        return true;
+      }
+    }
+  
+  
+    // This is a click listener for which cards have been selected
+    clickCount = id => {
+      this.state.cards.find((o, i) => {
+        if (o.id === id) {
+          if (cards[i].count === 0) {
+            cards[i].count = cards[i].count + 1;
+            this.setState({ score: this.state.score + 1 }, function () {
+              console.log(this.state.score);
+            });
+            this.state.cards.sort(() => Math.random() - 0.5)
+            return true;
+          }
+          else if (cards[i].count === 8) {
+  
+            this.setState({ score: this.state.score }, function () {
+              console.log("you won!");
+            });
+            this.state.cards.sort(() => Math.random() - 0.5)
+            return true;
+          }
+          else {
+            this.gameOver();
+  
+          }
+  
+        }
+      });
+    }
   
   
     // Map over this.state.cards and render a card component for each card object
@@ -22,7 +83,7 @@ class App extends Component {
         <div className="contents">
 
         {/* Header.js component */}
-          <Header score={this.state.score} highscore={this.state.highscore}>Marvel Match</Header>
+          <Header score={this.state.score} highscore={this.state.highscore}>Super Select</Header>
           
           {/* container class creates styling for the rows of cards */}
           <div className="container">
